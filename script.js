7ceea055001855beab004f9b797e4faa01496db4
VictorLira <victorliradev@gmail.com>
2021-05-17 15:39:10 -0300

    loan functinality

--- script.js
+++ script.js
@@ -6,6 +6,35 @@
 
 // Data
 
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+
 const accounts = [account1, account2, account3, account4];
 
 // Elements
@@ -144,17 +173,35 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    //add movement
+    currentAccount.movements.push(amount);
+
+    //update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+})
+
 btnClose.addEventListener('click', function(e) {
   e.preventDefault();
+
   if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
     const index = accounts.findIndex(acc => acc.username === currentAccount.username);
-    
 
+    //delete account
     accounts.splice(index, 1);
+
+    //hide UI
+    containerApp.style.opacity = 0;
   }
 
+  inputCloseUsername.value = inputClosePin.value = '';
 })
 
 const movements = [200, 450, -400, 3000, -650, 130, 70, 1300];
 
-console.log(accounts);
